5b4897f1b056f939e877832a68ae817fd8e2d49f
sindhuri <alladisindhuri@gmail.com>
2026-01-01 02:05:29 +0530

    Updated the compartment file, added code for sub-compartment (Flip-network-compartment) creation

--- compartment.tf
+++ compartment.tf
@@ -10,3 +10,16 @@ resource "oci_identity_compartment" "Flip-compartment" {
     }
 
 }
+
+# Create Sub-Compartment Flip-network-compartment in the Flip-compartment (Application compartment).
+
+resource "oci_identity_compartment" "Flip-network-compartment" {
+    name           = "Flip-network-compartment" # Name of the compartment
+    description    = "Compartment for Flip network resources" # Description of the compartment
+    compartment_id = "ocid1.compartment.oc1..aaaaaaaaxj2ideukw2bvru34qxssc5rvw75np4mk4bf3wfhcuarcab64laba" # Flip-compartment OCID(parent compartment OCID)
+   
+    defined_tags   = {
+        "Flip_Application.env"="prod"
+    }
+
+}
