57e27dd70a21b851a80a295b0a7f3c2b0aa05a61
sindhuri <alladisindhuri@gmail.com>
2026-01-01 01:34:18 +0530

    Updated oci-provider file with correct private key path

--- oci-provider.tf
+++ oci-provider.tf
@@ -4,6 +4,6 @@ provider "oci" {
   tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaa6o4u5tdt3hucwcqnbr4orzmthcv5n4kqel3abnexdjpz4l744pra"
   user_ocid        = "ocid1.user.oc1..aaaaaaaadyw2oor2sfnaleoiu7sklpjtlzxv4wwaqmqesgrsohcwz47h42sq"
   fingerprint      = "75:4a:cf:47:60:74:1f:1d:05:04:0f:8a:9b:ce:85:13"
-  private_key_path = "/D:/Virtual_Machines/OCI/Terraform_Keys/OCITerraformPrivate.pem"
+  private_key_path = "D:/Virtual_Machines/OCI/Terraform_Keys/OCITerraformPrivate.pem"
   region           = "us-ashburn-1"
 }
