fe45e75254233c5b2a2717b77e3c629565901ec7
sindhuri <alladisindhuri@gmail.com>
2026-01-02 02:14:32 +0530

    Created subnets.tf and added code for public subnet.

---
+++ subnets.tf
@@ -0,0 +1,17 @@
+# flip-vcn public subnet
+resource "oci_core_subnet" "public_subnet" {
+  compartment_id      = "ocid1.compartment.oc1..aaaaaaaaa3iwusb6nkibnr6lhi54oqdxzwr5awarpzl66gkoavowd35435bq"
+  vcn_id              = "ocid1.vcn.oc1.iad.amaaaaaanzgirfaa6nrxo6nrf3dvttchfvi4gkko3ubx73mv53eguinj62xq"
+  display_name        = "flip-public-subnet"
+  cidr_block          = "10.0.2.0/26"
+  dns_label           = "flippublicsubnet"
+  
+  route_table_id      = "ocid1.routetable.oc1.iad.aaaaaaaadb77t4phfsmre7xuxzjfzmgue7elkgembeh5nnkfl7dij647xmua"
+  security_list_ids   = ["ocid1.securitylist.oc1.iad.aaaaaaaahswwmtvqxxpd6aqsjzu42jghnjfs6fc2cgw5f7lnpsiechpqqlaa"]
+  
+  prohibit_public_ip_on_instance = false
+
+  defined_tags = {
+    "Flip_Application.env" = "prod"
+  }
+}
