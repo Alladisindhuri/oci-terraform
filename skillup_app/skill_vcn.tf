a707d75cd6278dd8088a6247529a7961489e6719
sindhuri <alladisindhuri@gmail.com>
2026-01-22 12:46:43 +0530

    Create Skillup app VCN

---
+++ skillup_app/skill_vcn.tf
@@ -0,0 +1,12 @@
+# Create a Virtual Cloud Network (VCN) Spoke in Skillup_network_compartment
+resource "oci_core_virtual_network" "Skillup_vcn" {
+  compartment_id = oci_identity_compartment.Skillup_network_compartment.id  # Skillup_network_compartment OCID
+  display_name   = "Skillup_vcn"
+  cidr_block     = "10.0.3.0/24"
+  dns_label      = "skillupvcn"
+
+  defined_tags = {
+    "Skillup_tags.env" = "prod"
+    "Skillup_tags.owner" = "sindhuri"
+  }
+}
