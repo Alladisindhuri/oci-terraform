a839092f14631df17555d8508dd69c251b30700b
sindhuri <alladisindhuri@gmail.com>
2026-01-22 12:25:56 +0530

    Create Skillup network, compute&storage compartments

--- skillup_app/skill_compartments.tf
+++ skillup_app/skill_compartments.tf
@@ -9,3 +9,31 @@ resource "oci_identity_compartment" "Skillup_compartment" {
   }
 
 }
+
+# Create Sub-Compartment Skillup_network_compartment in the Skillup_compartment (Application compartment).
+
+resource "oci_identity_compartment" "Skillup_network_compartment" {
+  name           = "Skillup_network_compartment" # Name of the Sub compartment
+  description    = "Compartment for Skillup network resources" # Description of the Sub compartment
+  compartment_id = oci_identity_compartment.Skillup_compartment.id # Skillup-compartment OCID(parent compartment OCID)
+
+  defined_tags = {
+    "Skillup_tags.env" = "prod"
+    "Skillup_tags.owner"= "sindhuri"
+  }
+
+}
+
+# Create Sub-Compartment Skillup_Compute_and_Storage_compartment in the Skillup_compartment (Application compartment).
+
+resource "oci_identity_compartment" "Skillup_Compute_and_Storage_compartment" {
+  name           = "Skillup_Compute_and_Storage_compartment"  # Name of the Sub compartment
+  description    = "Compartment for Skillup compute and storage resources"  # Description of the Sub compartment
+  compartment_id = oci_identity_compartment.Skillup_compartment.id # Skillup-compartment OCID(parent compartment OCID)
+
+  defined_tags = {
+    "Skillup_tags.env" = "prod"
+    "Skillup_tags.owner"= "sindhuri"
+  }
+
+}
