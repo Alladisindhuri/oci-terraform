bb40fb9e2ff2ea7f7e6a68183e7b51d30b48a5aa
sindhuri <alladisindhuri@gmail.com>
2026-01-22 16:03:58 +0530

    Create Skillup app route table.

---
+++ skillup_app/skill_route_table.tf
@@ -0,0 +1,19 @@
+resource "oci_core_route_table" "Skillup_vcn_app_rt" {
+  compartment_id = oci_identity_compartment.Skillup_network_compartment.id  #Skillup_network-compartment OCID
+  vcn_id         = oci_core_virtual_network.Skillup_vcn.id
+  display_name   = "Skillup_vcn_app_rt"
+
+  route_rules {
+    destination       = "10.0.0.0/24" # Hub VCN CIDR
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = "ocid1.drg.oc1.iad.aaaaaaaamj5tqzsegctlhrpxqvadcz34zyakj77xd27wotkys6ylx6wcddhq" # DRG OCID
+    description = "Route to Hub VCN via DRG"
+    }
+
+  defined_tags = {
+    "Skillup_tags.env" = "prod"
+    "Skillup_tags.owner" = "sindhuri"
+  }                     
+
+  
+}
