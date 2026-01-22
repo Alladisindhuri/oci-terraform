34d860e59c4fc7c99259f2818b089116c4cc2be9
sindhuri <alladisindhuri@gmail.com>
2026-01-22 12:13:41 +0530

    Create Skillup_compartment

---
+++ skillup_app/skill_compartments.tf
@@ -0,0 +1,11 @@
+resource "oci_identity_compartment" "Skillup_compartment" {
+  name           = "Skillup_compartment"  # Name of the compartment
+  description    = "Compartment for Skillup application resources" # Description of the compartment
+  compartment_id = "ocid1.tenancy.oc1..aaaaaaaa6o4u5tdt3hucwcqnbr4orzmthcv5n4kqel3abnexdjpz4l744pra" # Root compartment OCID
+
+  defined_tags = {
+    "Skillup_tags.env" = "prod"
+    "Skillup_tags.owner"= "sindhuri"
+  }
+
+}
