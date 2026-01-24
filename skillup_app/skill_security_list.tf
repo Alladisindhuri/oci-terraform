596e27388d5004c658818829b33675793f19cc3d
sindhuri <alladisindhuri@gmail.com>
2026-01-24 11:00:52 +0530

    Allow SSH traffic from Flip app to Skill app in SL.

--- skillup_app/skill_security_list.tf
+++ skillup_app/skill_security_list.tf
@@ -15,6 +15,18 @@ resource "oci_core_security_list" "Skillup_sl" {
 
 
   }
+  ingress_security_rules {
+    protocol    = "6"   # 6 is the protocol number for TCP
+    source      = "10.0.2.0/29"
+    source_type = "CIDR_BLOCK"
+    description = "Allow SSH from Flip app Subnet"
+    tcp_options {
+      min = 22
+      max = 22
+    }
+
+
+  }
 
   egress_security_rules {
     protocol    = "all"
