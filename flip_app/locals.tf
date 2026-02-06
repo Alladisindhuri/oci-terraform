c449f9ce7c2b2b8ffac6d2ec4b8a2f07087b97d0
sindhuri <alladisindhuri@gmail.com>
2026-02-06 23:16:56 +0530

    Create locals.tf for both flip and skillup

---
+++ flip_app/locals.tf
@@ -0,0 +1,6 @@
+locals{
+hub_compartment_ocid = "ocid1.compartment.oc1..aaaaaaaa3yn6ti4tzodgoncmxva3y7sq4szis4yn6q32psm7d6sscho26fxa" #hub compartment OCID
+hub_vcn_ocid = "ocid1.vcn.oc1.iad.amaaaaaanzgirfaaniddguce72cfu6uv3ygkfznnelp4vgaibbgxtv6wyx4a" #hub VCN OCID
+
+
+}
