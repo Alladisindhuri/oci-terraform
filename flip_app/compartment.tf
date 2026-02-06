a38f5e4094bcf94eaf6b3935b5851f18e42888a9
sindhuri <alladisindhuri@gmail.com>
2026-02-06 23:15:40 +0530

    Remove hardcoding values

--- flip_app/compartment.tf
+++ flip_app/compartment.tf
@@ -3,7 +3,7 @@
 resource "oci_identity_compartment" "Flip-compartment" {
   name           = "Flip-compartment"                                                                # Name of the compartment
   description    = "Compartment for Flip application resources"                                      # Description of the compartment
-  compartment_id = "ocid1.tenancy.oc1..aaaaaaaa6o4u5tdt3hucwcqnbr4orzmthcv5n4kqel3abnexdjpz4l744pra" # Root compartment OCID
+  compartment_id = var.tenancy_ocid # Root compartment OCID/ Tenancy OCID
 
   defined_tags = {
     "Flip_Application.env" = "prod"
