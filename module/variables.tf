fc63e9aabd6235c150debeca36a669e02ef4bd87
sindhuri <alladisindhuri@gmail.com>
2026-02-07 16:31:03 +0530

    Add definitions for route_table_id & SL_id

--- module/variables.tf
+++ module/variables.tf
@@ -42,4 +42,13 @@ variable "subnet_dns_label" {
     description = "The DNS label for the subnet"
     type        = string
     }
-    
+
+variable "subnet_route_table_id" {
+    description = "The OCID of the route table to associate with the subnet"
+    type        = string
+    }
+
+variable "subnet_security_list_ids" {
+    description = "A list of OCIDs of security lists to associate with the subnet"
+    type = string
+}
