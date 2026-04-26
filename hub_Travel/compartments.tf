c4bf8dfe4c40f2132f3b84d698a217d501e8ebe3
sindhuri <alladisindhuri@gmail.com>
2026-04-26 20:14:55 +0530

    create hub_Travel compartment

---
+++ hub_Travel/compartments.tf
@@ -0,0 +1,6 @@
+module "travelApp" {
+  source = "../module/compartments" # Path to the compartments module
+  compartment_name = "TravelApp" # Name of the compartment
+  compartment_description = "Compartment for Travel Application" # Description of the compartment
+  tenancy_ocid =  "var.tenancy_ocid" # OCID of the tenancy
+}
