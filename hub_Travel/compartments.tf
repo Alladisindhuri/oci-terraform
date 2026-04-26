28e306e91a68334da6ad711750482d2f01159c2e
sindhuri <alladisindhuri@gmail.com>
2026-04-26 21:01:20 +0530

    modified parent comaprtment id

--- hub_Travel/compartments.tf
+++ hub_Travel/compartments.tf
@@ -2,5 +2,5 @@ module "travelApp" {
   source = "../module/compartments" # Path to the compartments module
   compartment_name = "TravelApp" # Name of the compartment
   compartment_description = "Compartment for Travel Application" # Description of the compartment
-  tenancy_ocid =  "var.tenancy_ocid" # OCID of the tenancy
+  parent_compartment_ocid =  var.tenancy_ocid # OCID of the tenancy
 }
