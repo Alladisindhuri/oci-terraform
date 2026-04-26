91c4709b97bddfa5139b9652f5746eafc33c829f
sindhuri <alladisindhuri@gmail.com>
2026-04-26 22:59:38 +0530

    creating hub_TA_compute&storage compartment

--- hub_travel_app/compartments.tf
+++ hub_travel_app/compartments.tf
@@ -1,6 +1,13 @@
-module "travelApp" {
+module "hub_travelApp" {
   source = "../module/compartments" # Path to the compartments module
   compartment_name = "hub_travelApp" # Name of the compartment
   compartment_description = "Hub Compartment for Travel Application" # Description of the compartment
   parent_compartment_ocid =  var.tenancy_ocid # OCID of the tenancy
+}
+
+module "hub_TA_compute&storage" {
+  source = "../module/compartments" # Path to the compartments module
+  compartment_name = "hub_TA_compute&storage" # Name of the compartment
+  compartment_description = "Hub compute&storage Compartment for Travel Application" # Description of the compartment
+  parent_compartment_ocid =  module.hub_travelApp.compartment_id # OCID of the hub_travelApp compartment
 }
