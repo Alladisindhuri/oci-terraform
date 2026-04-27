99d3b813641c2f02ffde4bc5701cf685ec01f510
sindhuri <alladisindhuri@gmail.com>
2026-04-28 01:29:09 +0530

    modify vcn dns

--- hub_travel_app/vcn.tf
+++ hub_travel_app/vcn.tf
@@ -3,5 +3,5 @@ module "hub_TA_VCN" {
     vcn_display_name = "hub_TA_VCN"
     vcn_compartment_id = module.hub_TA_network.compartment_id # hub_TA_network compartment OCID
     vcn_cidr_block = "10.1.0.0/24"
-    vcn_dns_label = "hub-TA-VCN"
+    vcn_dns_label = "hubTAvcn"
 }
